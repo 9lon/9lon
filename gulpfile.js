92879ab60b46df665406b8f9a0d3f72763e359b1
Siripond laptop <pongpanot.t@nexts-corp.com>
2016-08-28 01:26:05 +0700

    ok dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('default',function(){
+gulp.task('build',function(){
      gulp.src(['src/nx-system.html','src/nx-param.html'])
-    .pipe(
-        $.replace('../bower_components/','../../')
-    )
-    .pipe($.crisper({scriptInHead:false}))
+    .pipe($.replace('../bower_components/','../../'))
+    .pipe($.crisper())
     .pipe($.sourcemaps.init())
     .pipe($.if('*.js',$.babel({ presets: ['es2015'] })))
     .pipe($.if('*.js',$.uglify()))
@@ -16,3 +14,9 @@ gulp.task('default',function(){
     gulp.src(['src/nx-behavior.html','src/init-system.html']).pipe(gulp.dest('dist/'));
 
 });
+
+gulp.task('watch',function(){
+    gulp.watch('src/nx-system.html',['build']);
+});
+
+gulp.task('default',['build']);
